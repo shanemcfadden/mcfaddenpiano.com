fc0e8c98db125105bd79f39c2b2b2614294d8f29
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-11 15:37:34 -0600

    Init youtube embed

--- src/pages/listen.js
+++ src/pages/listen.js
@@ -2,11 +2,12 @@ import * as React from 'react';
 import PageLayout from '../components/PageLayout';
 
 const ListenPage = () => {
-    return (
-        <PageLayout>
-            <div> Listen Page </div>
-        </PageLayout>
-    );
+  return (
+    <PageLayout>
+      <div> Listen Page </div>
+      <iframe src="https://www.youtube.com/embed/8jr2z99YjmI"></iframe>
+    </PageLayout>
+  );
 };
 
 export default ListenPage;
