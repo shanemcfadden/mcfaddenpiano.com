da5f5640854e3139c9238f833ed89c61d01d5fd5
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-11 10:00:25 -0600

    New linting

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -4,18 +4,19 @@ import NavBar from './NavBar';
 import 'normalize.css';
 import '../styles/styles.scss';
 
-const PageLayout = ({children}) => {
-    return (
-        <div className="content-container">
-            <h1>Shane McFadden</h1>
-            <h2>Collaborative Pianist</h2>
-            <NavBar /> 
-            {children}
-        </div>
-    );
+const PageLayout = ({ children }) => {
+  return (
+    <div className="content-container">
+      <h1>Shane McFadden</h1>
+      <h2>Collaborative Pianist</h2>
+      <NavBar />
+      {children}
+    </div>
+  );
 };
 
 PageLayout.propTypes = {
-    children: PropTypes.oneOfType([arrayOf(PropTypes.node), PropTypes.node]).isRequired
+  children: PropTypes.oneOfType([arrayOf(PropTypes.node), PropTypes.node])
+    .isRequired,
 };
 export default PageLayout;
