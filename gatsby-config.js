d48df03e6be79ccdb52ede7db38a6c2e3e8b6cfc
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-11 10:59:32 -0600

    Add Image querying Add .float--right-half Add image and lorem ipsum to index.html

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
 module.exports = {
   siteMetadata: {
     title: 'mcfaddenpiano.com',
   },
   plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: path.join(__dirname, 'src', 'images'),
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
