d08a675e203be3b6034e5cf1b67a5d9bb2c71aa5
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-12 00:17:58 -0600

    Init contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,45 @@ import * as React from 'react';
 import PageLayout from '../components/PageLayout';
 
 const ContactPage = () => {
-    return (
-        <PageLayout>
-            <div> Contact Page </div>
-        </PageLayout>
-    );
+  return (
+    <PageLayout>
+      <div> Contact Page </div>
+      <form
+        name="contact"
+        method="POST"
+        data-netlify-recaptcha="true"
+        netlify-honeypot="false-field"
+        data-netlify="true"
+      >
+        <p className="hidden">
+          <input name="false-field" />
+        </p>
+        <p>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Subject: <input type="text" name="subject" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </PageLayout>
+  );
 };
 
 export default ContactPage;
