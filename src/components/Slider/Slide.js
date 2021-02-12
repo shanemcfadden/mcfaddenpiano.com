9890185d490c22b1349e7b39c5d1fc7572b8f6b1
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-12 16:34:44 -0600

    Refactor Slide style

--- src/components/Slider/Slide.js
+++ src/components/Slider/Slide.js
@@ -4,13 +4,9 @@ import PropTypes from 'prop-types';
 const Slide = ({ content }) => {
   return (
     <div
+      className="slider__slide"
       style={{
-        height: '100%',
-        width: '100%',
         backgroundImage: `url('${content}')`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
       }}
     ></div>
   );
