ca4eaf5cbc4123052cd92b877f7a52656b9d51bf
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-19 14:19:40 -0600

    Refactor reducer w/ util.js

--- src/components/Slider/sliderReducer.js
+++ src/components/Slider/sliderReducer.js
@@ -1,3 +1,8 @@
+import {
+  getLoadedSlides,
+  getTranslateFactor,
+  getVisibleSlideIndex,
+} from './util';
 export default function reducer(state, action) {
   switch (action.type) {
     case 'browserWidth':
@@ -24,7 +29,7 @@ export default function reducer(state, action) {
         transition: 0,
         translate:
           getVisibleSlideIndex(action.slides.length) * state.browserWidth,
-        loadedSlides: loadedSlides(action.slides, state.activeSlideIndex),
+        loadedSlides: getLoadedSlides(action.slides, state.activeSlideIndex),
       };
     case 'transition':
       return { ...state, transition: action.value };
@@ -33,36 +38,4 @@ export default function reducer(state, action) {
     default:
       throw new Error('Invalid action type');
   }
-}
-
-function loadedSlides(slidesArr, activeIndex) {
-  const loaded = [];
-  const numberOfSlides = slidesArr.length;
-  let i = (activeIndex + Math.floor(numberOfSlides / 2) + 1) % numberOfSlides;
-  while (loaded.length < numberOfSlides) {
-    if (i === numberOfSlides) i = 0;
-    loaded.push(slidesArr[i++]);
-  }
-  return loaded;
-}
-
-function getTranslateFactor(
-  activeSlideIndex,
-  newSlideIndex,
-  loadedSlidesLength
-) {
-  const lowestPossibleTranslateFactor =
-    -1 * getVisibleSlideIndex(loadedSlidesLength);
-  const highestPossibleTranslateFactor = Math.floor(loadedSlidesLength / 2);
-  const possibleTranslate = newSlideIndex - activeSlideIndex;
-
-  if (possibleTranslate < lowestPossibleTranslateFactor)
-    return possibleTranslate + loadedSlidesLength;
-  if (possibleTranslate > highestPossibleTranslateFactor)
-    return possibleTranslate - loadedSlidesLength;
-  return possibleTranslate;
-}
-
-function getVisibleSlideIndex(slidesLength) {
-  return Math.floor((slidesLength - 1) / 2);
 }
