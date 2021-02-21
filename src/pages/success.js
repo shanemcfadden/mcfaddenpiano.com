be444c4749b04da56069c64db684ed1b55d7f9e3
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2021-02-21 15:06:55 -0600

    Add vertical centering

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,10 +6,12 @@ import PageLayout from '../components/PageLayout';
 const SuccessPage = ({ location }) => {
   return (
     <PageLayout location={location}>
-      <div className="center-text">
-        <h1>Success!</h1>
-        <p>Your form has been submitted. </p>
-        <Link to="/">Return to the homepage.</Link>
+      <div className="container--center-content-vertically">
+        <div className="center-text">
+          <h1>Success!</h1>
+          <p>Your form has been submitted. </p>
+          <Link to="/">Return to the homepage.</Link>
+        </div>
       </div>
     </PageLayout>
   );
