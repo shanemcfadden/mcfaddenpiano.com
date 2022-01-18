12fadcd60158d8848df269a35281e88cc578f17c
Shane McFadden <28913393+shanemcfadden@users.noreply.github.com>
2022-01-17 21:12:04 -0600

    Add linting to CI

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,28 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true,
-        'node': true
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  ignorePatterns: ['node_modules/**', 'public/**', 'cache/**'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended'
-    ],
-    'globals': {
-        'Atomics': 'readonly',
-        'SharedArrayBuffer': 'readonly'
-    },
-    'ignorePatterns': ['node_modules/**', 'public/**', 'cache/**'],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 2018,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        'prettier'
-    ],
-    rules: {
-        'prettier/prettier': 'error'
-    }
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
