a7772e4e0ed6e1aaab93f7f2f35249b71ecc8d31
Shane McFadden <shane.t.mcfadden@gmail.com>
2025-10-09 21:11:18 -0500

    Add theme color (#233)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -17,5 +17,6 @@ export const Head: HeadFC = () => (
     <html lang="en" />
     <body className="bg-dark text-white font-[Mukta]" />
     <title>Not found</title>
+    <meta name="theme-color" content="#111111" />
   </>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,7 +67,7 @@ export const Head: HeadFC<{
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#da532c" />
-      <meta name="theme-color" content="#ffffff" />
+      <meta name="theme-color" content="#111111" />
       {/* end real favicon generator info */}
 
       <meta itemProp="name" content={title} />
